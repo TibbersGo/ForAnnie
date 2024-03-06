b5ab74c97b1c1d75e141d963760a3d02261b454d
TibbersGo <1025928240@qq.com>
2024-03-06 21:10:15 +0800

    commit new code

--- find_target_node/main.go
+++ find_target_node/main.go
@@ -1,5 +1,36 @@
 package main
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 func main() {
 
+}
+
+func findTargetNode(root *TreeNode, cnt int) int {
+	// 中序遍历后倒序的第cnt个节点
+	//
+
+	return 0
+}
+
+var cnt int
+var res int
+
+func dfs(node *TreeNode, cnt int) {
+	if node == nil {
+		return
+	}
+	dfs(node.Right, cnt)
+	if cnt == 0 {
+		return
+	}
+	cnt -= 1
+	if cnt == 0 {
+		res = node.Val
+	}
+	dfs(node.Left, cnt)
 }
