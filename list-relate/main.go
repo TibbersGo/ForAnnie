4e1f6f179870c95c14d518dbeabe6c491d282494
TibbersGo <1025928240@qq.com>
2024-03-10 11:33:23 +0800

    commit code

--- list-relate/main.go
+++ list-relate/main.go
@@ -1,5 +1,20 @@
 package main
 
+type ListNode struct {
+	Val      int
+	NextNode *ListNode
+}
+
 func main() {
 
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	tmp := head
+	for tmp.NextNode != nil {
+
+	}
 }
